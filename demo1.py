518561ccfb9dd5730ff83d0d591473d70679d2b9
Nancy <er.nancy001@gmail.com>
2022-01-28 18:22:21 +0530

    first commit via terminal

--- demo1.py
+++ demo1.py
@@ -1,2 +1,3 @@
 print("Hello to Python")
 print("Changed by mainCopy")
+print("Changed by Clone")
---
+++ new1.swift
@@ -0,0 +1,9 @@
+//
+//  new1.swift
+//  
+//
+//  Created by Nancy Kansal on 28/01/22.
+//
+
+import Foundation
+print("This is a swift file")
